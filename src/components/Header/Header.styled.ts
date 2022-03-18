0d3425858eb23d5a453449dafb94a6925332df58
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-18 03:13:07 +0200

    style: adding the base styling for the styled header

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+import { flexMixin } from '../../utils/mixins';
+
+const StyledHeader = styled.header`
+    & > nav {
+        width: 75%;
+        height: 100%;
+        margin: auto;
+        ${flexMixin('space-between', 'flex-end', 'row')};
+    }
+    & > container {
+        height: 50%;
+    }
+    h1 {
+        padding: 1rem 0rem;
+    }
+`;
+
+export default StyledHeader;

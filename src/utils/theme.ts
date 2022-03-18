18e86f0386a7774b0b9756ad6bfcaad1095393f8
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-18 02:39:15 +0200

    style: adding the baase theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'styled-components';
+
+const myTheme: DefaultTheme = {
+    colors: {
+        brand: '#f43f5e',
+        dark: '#1e293b',
+        lowerDark: '#343e4f',
+        mid: '#475569',
+        light: '#f1f5f9',
+        lightRGB: '241, 245, 249',
+    },
+};
+
+export { myTheme };

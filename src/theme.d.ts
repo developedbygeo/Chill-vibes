44cdc61acb41082f56cb73128442156bb8488d67
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-18 02:37:46 +0200

    chore: adding theme declaration

---
+++ src/theme.d.ts
@@ -0,0 +1,15 @@
+import 'styled-components';
+import {} from 'styled-components/cssprop';
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        colors: {
+            brand: string;
+            dark: string;
+            lowerDark: string;
+            mid: string;
+            light: string;
+            lightRGB: string;
+        };
+    }
+}

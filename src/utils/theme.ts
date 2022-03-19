403b36ae58c380768cd82575c4deb0942f27edae
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-20 00:33:44 +0200

    chore: adding new dark rgb clr

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -4,6 +4,7 @@ const myTheme: DefaultTheme = {
     colors: {
         brand: '#f43f5e',
         dark: '#1e293b',
+        darkRGB: '30, 41, 59',
         lowerDark: '#343e4f',
         mid: '#222A39',
         light: '#f1f5f9',

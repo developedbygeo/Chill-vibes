0e982e0e3378bf2dfa69fc8a26d091e0c6b9ad36
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-20 00:26:30 +0200

    feat: adding the updateSongDetails and skipSong reducers

--- src/app/trackSlice.ts
+++ src/app/trackSlice.ts
@@ -6,6 +6,10 @@ const cartInit = {
     data,
     currentTrack: data[0],
     isCurrentTrackPlaying: false,
+    songDetails: {
+        current: 0,
+        duration: 0,
+    },
 };
 
 const trackSlice = createSlice({
@@ -23,9 +27,26 @@ const trackSlice = createSlice({
         setIsPlaying: (state) => {
             state.isCurrentTrackPlaying = !state.isCurrentTrackPlaying;
         },
+        updateSongDetails: (state, action) => {
+            state.songDetails.current = action.payload.current;
+            if (action.payload.duration) {
+                state.songDetails.duration = action.payload.duration;
+            }
+        },
+        skipSong: (state, action) => {
+            const currentIndex = state.data.findIndex((track) => track.id === state.currentTrack.id);
+            const nextTrack =
+                action.payload === 'forward'
+                    ? data[(currentIndex + 1) % data.length]
+                    : data[(currentIndex - 1) % data.length] || data[0];
+            const dataTrack = state.data.find((track) => track.id === nextTrack.id);
+            state.data.forEach((track) => (track.active = false));
+            if (dataTrack) dataTrack.active = true;
+            state.currentTrack = nextTrack;
+        },
     },
 });
 
-export const { setActive, setIsPlaying } = trackSlice.actions;
+export const { setActive, setIsPlaying, updateSongDetails, skipSong } = trackSlice.actions;
 
 export default trackSlice.reducer;

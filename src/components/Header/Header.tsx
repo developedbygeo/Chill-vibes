90dc80fd49cfef919104af603d8ade911f8fa9d3
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-20 00:27:07 +0200

    feat: wiring up the button with the toggle

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,17 @@
+import { useAppDispatch } from '../../app/store';
+import { toggleLibrary } from '../../app/uiSlice';
+
 import StyledHeader from './Header.styled';
 import { CtaButton } from '../shared/Button.styled';
 import { MdLibraryMusic } from 'react-icons/md';
 
 const Header = () => {
+    const dispatch = useAppDispatch();
+
+    const toggleLibraryHandler = () => {
+        dispatch(toggleLibrary());
+    };
+
     return (
         <StyledHeader>
             <nav>
@@ -10,7 +19,7 @@ const Header = () => {
                     <h1>Chill Vibes</h1>
                 </div>
                 <div className="container">
-                    <CtaButton includesSVG>
+                    <CtaButton onClick={toggleLibraryHandler} includesSVG>
                         <span>Library</span>
                         <MdLibraryMusic />
                     </CtaButton>

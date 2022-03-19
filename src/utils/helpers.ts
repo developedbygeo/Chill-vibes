dc6c132fbdcf670dbc516beaef8243147d94fa53
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-20 00:33:18 +0200

    refactor: adding the playTrack helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,14 @@
 export const formatTime = (time: number) => {
     return Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2);
 };
+
+export const playTrack = (isTrackPlaying: boolean, audioRef: React.RefObject<HTMLAudioElement>): void => {
+    if (isTrackPlaying && audioRef.current) {
+        const playPromise = audioRef.current.play();
+        if (!playPromise !== undefined) {
+            playPromise.then((audio) => {
+                audioRef.current && audioRef.current.play();
+            });
+        }
+    }
+};

3ab56a2a5c9bbde353234aa4334ceb321bbde028
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-20 00:29:30 +0200

    feat: adding the selectTrackHandler and passing props

--- src/components/Library/Library.tsx
+++ src/components/Library/Library.tsx
@@ -1,17 +1,36 @@
-import { useAppSelector } from '../../app/hooks';
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { setActive } from '../../app/trackSlice';
 
+import { playTrack } from '../../utils/helpers';
+
+import { BaseProps } from '../../utils/models/props.model';
 import StyledLibrary from './Library.styled';
 import LibraryTrack from './LibraryTrack';
 
-const Library = () => {
+const Library = ({ audioRef }: BaseProps) => {
+    const isLibraryShown = useAppSelector((state) => state.ui.showLibrary);
     const allTracks = useAppSelector((state) => state.tracks.data);
+    const isTrackPlaying = useAppSelector((state) => state.tracks.isCurrentTrackPlaying);
+
+    const dispatch = useAppDispatch();
+
+    const selectTrackHandler = (id: string) => {
+        dispatch(setActive(id));
+        if (audioRef && audioRef.current) {
+            playTrack(isTrackPlaying, audioRef);
+        }
+    };
 
     return (
-        <StyledLibrary>
-            <h2>Library</h2>
+        <StyledLibrary className={`${isLibraryShown ? 'visible' : ''}`}>
+            <h2>My Library</h2>
             <ul>
                 {allTracks.map((track) => (
-                    <LibraryTrack track={track} key={track.id} />
+                    <LibraryTrack
+                        track={track}
+                        key={track.id}
+                        onTrackSelection={selectTrackHandler.bind(null, track.id)}
+                    />
                 ))}
             </ul>
         </StyledLibrary>

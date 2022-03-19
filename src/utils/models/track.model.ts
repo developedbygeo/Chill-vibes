a2b3de8b7e78a5d758d03297bc42841e5a77f4fe
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-19 02:14:19 +0200

    chore: adding the track type

---
+++ src/utils/models/track.model.ts
@@ -0,0 +1,11 @@
+type TrackType = {
+    title: string;
+    artist: string;
+    cover: string;
+    id: string;
+    colors: string[];
+    audio: string;
+    active: boolean;
+};
+
+export default TrackType;

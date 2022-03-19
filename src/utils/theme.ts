30c1a5e0b6cb575741e04b4cb03f3e37d7d23a75
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-19 02:05:19 +0200

    style: changing mid clr

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -5,7 +5,7 @@ const myTheme: DefaultTheme = {
         brand: '#f43f5e',
         dark: '#1e293b',
         lowerDark: '#343e4f',
-        mid: '#475569',
+        mid: '#222A39',
         light: '#f1f5f9',
         lightRGB: '241, 245, 249',
     },

166a520ff472f24de824017e432aceca1dc4bb4c
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-20 00:25:46 +0200

    feat: adding library and audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
+import { useRef, Suspense } from 'react';
 import Header from './components/Header/Header';
+import Audio from './components/Player/Audio';
 import Controls from './components/Player/Controls';
 import Player from './components/Player/Details';
 import Library from './components/Library/Library';
 import GlobalStyle from './utils/globalStyle';
 
 function App() {
+    const audioRef = useRef<HTMLAudioElement>(null);
+
     return (
         <>
             <GlobalStyle />
             <Header />
             <main>
                 <Player />
-                <Controls />
+                <Controls audioRef={audioRef} />
             </main>
-            <Library />
+            <Library audioRef={audioRef} />
+            <Audio ref={audioRef} />
         </>
     );
 }

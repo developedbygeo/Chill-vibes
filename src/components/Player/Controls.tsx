1ea3cdbb835a7dfd4dbc20d3d7fb14d054dae037
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-19 02:11:36 +0200

    feat: adding the track controls component

--- src/components/Player/Controls.tsx
+++ src/components/Player/Controls.tsx
@@ -1,26 +1,72 @@
+import React, { useRef, useState } from 'react';
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { setIsPlaying } from '../../app/trackSlice';
+
+import { formatTime } from '../../utils/helpers';
+
 import { SecondaryButton } from '../shared/Button.styled';
 import { MdArrowBackIos, MdArrowForwardIos, MdPause, MdPlayArrow } from 'react-icons/md';
+import StyledControls from './Controls.styled';
 
 const Controls = () => {
+    const [songData, setSongData] = useState({ current: 0, duration: 0 });
+    const currentSong = useAppSelector((state) => state.tracks.currentTrack);
+    const isTrackPlaying = useAppSelector((state) => state.tracks.isCurrentTrackPlaying);
+    const dispatch = useAppDispatch();
+    const audioRef = useRef<HTMLAudioElement>(null);
+    const { audio } = currentSong;
+
+    const playButton = isTrackPlaying ? <MdPause className="small-icon" /> : <MdPlayArrow className="small-icon" />;
+
+    const playHandler = () => {
+        dispatch(setIsPlaying());
+        if (audioRef.current && isTrackPlaying) {
+            audioRef.current.pause();
+        }
+        if (audioRef.current && !isTrackPlaying) {
+            audioRef.current.play();
+        }
+    };
+
+    const timeUpdateHandler = (e: React.BaseSyntheticEvent) => {
+        const current = e.target.currentTime;
+        const duration = e.target.duration;
+        setSongData({ ...songData, current, duration });
+    };
+
+    const changeTimeHandler = (e: React.BaseSyntheticEvent) => {
+        if (audioRef.current) {
+            audioRef.current.currentTime = e.target.value;
+        }
+        setSongData({ ...songData, current: e.target.value });
+    };
+
     return (
-        <div className="controls">
-            <div className="time-control">
-                <p>Start Time</p>
-                <input type="range" name="song-duration" id="time" />
-                <p>End Time</p>
+        <StyledControls>
+            <div className="time">
+                <p>{formatTime(+songData.current)}</p>
+                <input
+                    min={0}
+                    max={songData.duration}
+                    value={songData.current}
+                    onChange={changeTimeHandler}
+                    type="range"
+                    name="song-duration"
+                    id="time"
+                />
+                <p>{formatTime(+songData.duration)}</p>
             </div>
-            <div className="play-control">
+            <div className="ctrl">
                 <SecondaryButton>
                     <MdArrowBackIos />
                 </SecondaryButton>
-                <SecondaryButton>
-                    <MdPlayArrow className="small-icon" />
-                </SecondaryButton>
+                <SecondaryButton onClick={playHandler}>{playButton}</SecondaryButton>
                 <SecondaryButton>
                     <MdArrowForwardIos />
                 </SecondaryButton>
             </div>
-        </div>
+            <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={audio} />
+        </StyledControls>
     );
 };
 

d9dffcac5fec923aec02c410cf20c44a923d6f0e
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-19 02:05:33 +0200

    chore: adding redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
 import { myTheme } from './utils/theme';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
         <ThemeProvider theme={myTheme}>
-            <App />
+            <Provider store={store}>
+                <App />
+            </Provider>
         </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root'),

4562e44a7fc9cfe1d7282e4f6d50777ccbf72f40
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-19 02:14:08 +0200

    chore: adding the base data

---
+++ src/utils/data.ts
@@ -0,0 +1,59 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const data = [
+    {
+        title: 'Seagulls',
+        artist: 'Misha, Zmeyev, Viktor Minsky',
+        cover: 'https://chillhop.com/wp-content/uploads/2022/01/5753da482a6839b31e4905b22a2f8d65913e7eb4-1024x1024.jpg',
+        id: uuidv4(),
+        colors: ['#76adc9', '#fdddc8'],
+        audio: 'https://mp3.chillhop.com/serve.php/?mp3=28936',
+        active: true,
+    },
+    {
+        title: 'Nomad',
+        artist: 'Aviino',
+        cover: 'https://chillhop.com/wp-content/uploads/2021/09/a2d710149876c8e0c9fb097840b2438fbc21bd37-1024x1024.jpg',
+        id: uuidv4(),
+        colors: ['#da8147', '#5c7a8c'],
+        audio: 'https://mp3.chillhop.com/serve.php/?mp3=23067',
+        active: false,
+    },
+    {
+        title: 'Tournament Arc',
+        artist: 'Sleepy Fish',
+        cover: 'https://chillhop.com/wp-content/uploads/2021/09/2ce75252f5419a45d76bb93424ac1eae3e688b17-1024x1024.jpg',
+        id: uuidv4(),
+        colors: ['#1d66aa', '#fec198'],
+        audio: 'https://mp3.chillhop.com/serve.php/?mp3=24824',
+        active: false,
+    },
+    {
+        title: 'On The Other Side',
+        artist: 'Hanz',
+        cover: 'https://chillhop.com/wp-content/uploads/2021/08/b0bb2309d0c8fe0a32907ecddab689501b7de5cf-1024x1024.jpg',
+        id: uuidv4(),
+        colors: ['#c58184', '#80a8ec'],
+        audio: 'https://mp3.chillhop.com/serve.php/?mp3=23067',
+        active: false,
+    },
+    {
+        title: 'Looking In',
+        artist: 'Smile High, Teddy Roxpin',
+        cover: 'https://chillhop.com/wp-content/uploads/2021/08/af3ce13ad39d38057f00144f8a7c295877ccfec1-1024x1024.jpg',
+        id: uuidv4(),
+        colors: ['#458b93', '#d76662'],
+        audio: 'https://mp3.chillhop.com/serve.php/?mp3=23335',
+        active: false,
+    },
+    {
+        title: 'Luz',
+        artist: 'Juan Rios, Ian Ewing',
+        cover: 'https://chillhop.com/wp-content/uploads/2021/07/4163ebb931e06d4ee8eb184295c0246d4a5055f7-1024x1024.jpg',
+        id: uuidv4(),
+        colors: ['#fd8a70', '#fdc997'],
+        audio: 'https://mp3.chillhop.com/serve.php/?mp3=21651',
+        active: false,
+    },
+];
+export default data;

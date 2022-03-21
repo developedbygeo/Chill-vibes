3e295d34ca15335b7fe79d43d115579e215cabc1
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-21 03:07:30 +0200

    refactor: typing playTrack correctly

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,7 +2,10 @@ export const formatTime = (time: number) => {
     return Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2);
 };
 
-export const playTrack = (isTrackPlaying: boolean, audioRef: React.RefObject<HTMLAudioElement>): void => {
+export const playTrack = async (
+    isTrackPlaying: boolean,
+    audioRef: React.RefObject<HTMLAudioElement>,
+): Promise<void> => {
     if (isTrackPlaying && audioRef.current) {
         const playPromise = audioRef.current.play();
         if (!playPromise !== undefined) {

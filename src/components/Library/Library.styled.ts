96bb6ab21fb2f8201f5ccc79d5125408429d4f16
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-21 03:03:15 +0200

    style: adding mq and adjusting the headers layout

--- src/components/Library/Library.styled.ts
+++ src/components/Library/Library.styled.ts
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 
+import { devices } from '../../utils/breakpoints';
 import { flexMixin, gridMixin, maxContainer } from '../../utils/mixins';
 
 const StyledLibrary = styled.aside`
     position: fixed;
     top: 0;
     left: 0;
-    width: 30rem;
+    width: 100%;
     height: 100%;
+    z-index: 20;
     box-shadow: 2px 2px 50px rgba(241, 245, 249, 0.05);
     background: ${({ theme }) => theme.colors.mid};
     ${gridMixin('1fr', '0.2fr 1fr')};
@@ -21,14 +23,17 @@ const StyledLibrary = styled.aside`
         transform: translateX(0%);
         opacity: 1;
     }
-    h2 {
-        padding: 2rem;
+
+    .aside-header {
+        ${flexMixin('space-between', 'center', 'row')};
+        padding: 2rem 1.5rem;
     }
 
     ul {
         ${flexMixin('space-around', 'flex-start', 'column')};
         ${maxContainer};
         gap: 0.5rem;
+        margin-top: 3.5vh;
     }
 
     li {
@@ -37,20 +42,22 @@ const StyledLibrary = styled.aside`
         gap: 2rem;
         padding: 2rem;
         cursor: pointer;
-        transition: all 100ms ease;
+        transition: all 250ms ease;
         & > img {
             width: 50%;
         }
         p {
             margin-top: 0.5rem;
             font-weight: 300;
-            font-size: 1.6rem;
+            font-size: 1.8rem;
         }
         h4 {
+            font-size: 2.3rem;
             font-weight: 500;
         }
         @media (hover: hover) {
             &:not(.playing):hover {
+                transition: all 200ms ease;
                 background: rgba(${({ theme }) => theme.colors.lightRGB}, 0.025);
             }
         }
@@ -76,6 +83,15 @@ const StyledLibrary = styled.aside`
     }
     .playing {
         background: ${({ theme }) => theme.colors.lowerDark};
+    }
+
+    @media ${devices.tablet} {
+        width: 30vw;
+        li {
+            & > img {
+                width: 25%;
+            }
+        }
     }
 `;
 

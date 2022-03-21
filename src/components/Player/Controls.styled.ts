f8564c30a665abc27b96b0fcf05e197f1463ca3c
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-21 03:04:00 +0200

    style: adding mq and adjusting width

--- src/components/Player/Controls.styled.ts
+++ src/components/Player/Controls.styled.ts
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 
+import { devices } from '../../utils/breakpoints';
 import { flexMixin } from '../../utils/mixins';
 
 const StyledControls = styled.section`
     min-height: 20vh;
-    ${flexMixin('space-between', 'center', 'column')};
+    width: 70%;
+    margin: auto;
+    ${flexMixin('space-around', 'center', 'column')};
 
     & > .time {
-        width: 50%;
+        width: 100%;
         display: flex;
         align-items: center;
         p {
@@ -16,9 +19,19 @@ const StyledControls = styled.section`
     }
     & > .ctrl {
         ${flexMixin('space-between', 'center', 'row')};
-        width: auto;
+        width: 100%;
         gap: 2rem;
         padding: 1rem;
+    }
+    @media screen and (min-width: 608px) {
+        & > .ctrl {
+            ${flexMixin('center', 'center', 'row')};
+        }
+    }
+    @media ${devices.tablet} {
+        & > .time {
+            max-width: 60vw;
+        }
     }
 `;
 export default StyledControls;

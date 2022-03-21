c10079b7daaae8b53c00e889b80b81090d044726
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-21 03:02:15 +0200

    refactor: handling the library state centrally

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { useRef, Suspense } from 'react';
+import { useRef, useState } from 'react';
 import Header from './components/Header/Header';
-import Audio from './components/Player/Audio';
 import Controls from './components/Player/Controls';
 import Player from './components/Player/Details';
 import Library from './components/Library/Library';
@@ -8,18 +7,20 @@ import GlobalStyle from './utils/globalStyle';
 
 function App() {
     const audioRef = useRef<HTMLAudioElement>(null);
+    const [libraryShows, setLibraryShows] = useState(false);
+
+    const libraryToggleHandler = () => setLibraryShows((prevState) => !prevState);
 
     return (
-        <>
+        <div className={`App ${libraryShows ? 'app-active' : ''}`}>
             <GlobalStyle />
-            <Header />
+            <Header onToggleLibrary={libraryToggleHandler} />
             <main>
                 <Player />
                 <Controls audioRef={audioRef} />
             </main>
-            <Library audioRef={audioRef} />
-            <Audio ref={audioRef} />
-        </>
+            <Library audioRef={audioRef} onToggleLibrary={libraryToggleHandler} isLibraryShown={libraryShows} />
+        </div>
     );
 }
 

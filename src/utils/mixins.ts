d2e3b77da35c6dbfa1441a67547a5d014c883496
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-22 03:28:22 +0200

    feat: adding shadow and underline snippets

--- src/utils/mixins.ts
+++ src/utils/mixins.ts
@@ -1,5 +1,9 @@
 import { css } from 'styled-components';
 
+export const signatureShadow = css`
+    box-shadow: 0px 5px 10px rgba(${({ theme }) => theme.colors.eerieBlack}, 0.9);
+`;
+
 export const gridMixin = (columns: string, rows: string, gap?: string) => css`
     display: grid;
     grid-template-columns: ${columns};
@@ -12,6 +16,31 @@ export const flexMixin = (justify: string, align: string, direction: string) => 
     flex-direction: ${direction};
     justify-content: ${justify};
     align-items: ${align};
+`;
+export const underlineEffect = css`
+    @media (hover: hover) {
+        &::after {
+            position: absolute;
+            content: '';
+            left: 0;
+            right: 0;
+            bottom: 0px;
+            width: 100%;
+            height: 0.75px;
+            transform-origin: right;
+            transform: scaleX(0);
+            background: ${({ theme }) => theme.colors.light};
+            transition: transform 350ms ease;
+        }
+        @media (hover: hover) {
+            &:hover {
+                &::after {
+                    transform-origin: left;
+                    transform: scaleX(1);
+                }
+            }
+        }
+    }
 `;
 
 export const maxContainer = css`

cd0431aaeea64ea4f4aff2c73dc73a9f3942225d
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-23 00:23:23 +0200

    fix: adjusting the progress bar

--- src/components/shared/Slider/CustomSlider.tsx
+++ src/components/shared/Slider/CustomSlider.tsx
@@ -10,10 +10,12 @@ type CustomSliderProps = {
 };
 
 const CustomSlider = ({ max, value, progress, gradient, onChange }: CustomSliderProps) => {
+    const adjustedProgress = progress + 0.25;
+
     return (
         <StyledInputWrapper style={{ background: `linear-gradient(to right, ${gradient[0]}, ${gradient[1]})` }}>
             <input min={0} max={max} value={value} onChange={onChange} type="range" name="song-duration" id="time" />
-            <div style={{ transform: `translateX(${progress}%)` }} className="animated-track"></div>
+            <div style={{ transform: `translateX(${adjustedProgress}%)` }} className="animated-track"></div>
         </StyledInputWrapper>
     );
 };

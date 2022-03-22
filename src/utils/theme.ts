67c3f2920e8caf76bd59b0d17bc5130c5c463603
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-22 03:28:35 +0200

    chore: updating theme

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -4,10 +4,11 @@ const myTheme: DefaultTheme = {
     colors: {
         brand: '#f43f5e',
         dark: '#1e293b',
-        darkRGB: '30, 41, 59',
-        lowerDark: '#343e4f',
+        secondaryDark: '#232d3e',
         mid: '#222A39',
+        lowerDark: '#343e4f',
         light: '#f1f5f9',
+        darkRGB: '30, 41, 59',
         lightRGB: '241, 245, 249',
         eerieBlack: '31, 31, 31',
         gray: '92, 92, 92',
